72646fb72b48257ef984403350c173ea91e041f2
unknown <noahbeshahb@gmail.com>
2023-08-31 04:57:42 -0700

    'star'

--- app/(Body_Section)/Section_3.tsx
+++ app/(Body_Section)/Section_3.tsx
@@ -26,7 +26,7 @@ function Section_3() {
                         <div className='flex text-black  flex-col space-y-5 mt-10'>
                         <input className='w-[300px] outline-none pl-5 h-[40px] rounded-xl' type="text" placeholder='Name'/>
                         <input className='w-[300px] outline-none h-[40px] rounded-xl pl-5' type="text" placeholder='Email'/>
-                        <textarea className='pl-5 outline-none' placeholder='Message' name="" id="" cols="30" rows="6"></textarea>
+                        <textarea className='pl-5 outline-none' placeholder='Message' name="" id="" ></textarea>
                         <button className='border border-white w-[150px] h-[40px] rounded-md text-white hover:bg-slate-500'>Send Message</button>
                         </div>
                     </form>
